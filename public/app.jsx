067c52436a4cdfdf887b9e7b0cab6d653be4a51c
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2016-09-06 00:40:36 +0800

    Component Properties exercies

--- public/app.jsx
+++ public/app.jsx
@@ -1,22 +1,27 @@
 var Greeter = React.createClass({
 	getDefaultProps: function (){
 		return {
-			name: 'React'
+			name: 'React',
+			msg: 'This is from a component!'
 		};
 	},
 	render: function(){
 		var name = this.props.name;
-		
+		var message = this.props.msg;
+
 		return (
 			<div>
 				<h1>Hello {name}!</h1>
-				<p>This is from a component!</p>
+				<p>{message}</p>
 			</div>
 		);
 	}
 });
 
+var firstName = 'Juan Cornelio';
+var customMessage = 'This is just a try.';
+
 ReactDOM.render(
-	<Greeter name="Juan Cornelio"/>,
+	<Greeter name={firstName} msg={customMessage}/>,
 	document.getElementById('app')
 );
