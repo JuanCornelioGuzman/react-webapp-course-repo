f4a3f4277a9ba3d6b12a9207e413c8f7205fb9d3
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2016-09-21 00:08:23 +0800

    Add ReactWeather

---
+++ ReactWeather/app/components/Main.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <h2>Main Component</h2>
+    );
+  }
+});
+
+module.exports = Main;
---
+++ ReactWeather/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve:{
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx'
+    },
+    extensions:['','.js','.jsx']
+  },
+  module : {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /{node_modules|bower_components}/
+      }
+    ]
+  }
+};
