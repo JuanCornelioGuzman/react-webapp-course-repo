ed626a6ff5501d6d8019a027b239fd522ae77b28
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2016-09-20 22:20:02 +0800

    Add alias for components

--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
-var GreeterMessage = require('./GreeterMessage');
-var GreeterForm = require('./GreeterForm');
+var GreeterMessage = require('GreeterMessage');
+var GreeterForm = require('GreeterForm');
 
 //Container Component - maintain state
 //It must only render child component
