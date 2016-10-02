c7a9ea58d7a4bc3f69e388f2597f2c9fb7e5166f
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2016-10-02 23:19:28 +0800

    [Refactoring] Change jsx files to arrow functions

--- ReactWeather/app/components/WeatherMessage.jsx
+++ ReactWeather/app/components/WeatherMessage.jsx
@@ -1,15 +1,12 @@
 var React = require('react');
 
 //Presentational Component - no state
-var WeatherMessage = React.createClass({
-	render: function () {
-		var {city,temp} = this.props;
-		return (
-			<div>
-				<p>It's {temp} degrees in {city}</p>
-			</div>
-		);
-	}
-});
+var WeatherMessage = ({city,temp}) => {
+	return (
+		<div>
+			<p>It's {temp} degrees in {city}</p>
+		</div>
+	);
+}
 
 module.exports = WeatherMessage;
